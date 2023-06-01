e5ad75801d9b25714a620a98269f4de749d806ca
Duc Anh Nguyen <ADnh23nguyen@gmail.com>
2023-06-01 07:59:39 +0700

    update statistic scanned

--- apis/api_scan.py
+++ apis/api_scan.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException
-from packages.database import SessionLocal, urls, scan_history, users
+from packages.database import SessionLocal, scan_history, users
 from datetime import datetime
 from prediction import predict
 import requests
 import re
+from sqlalchemy.sql import func, extract
 
 router = APIRouter()
 db = SessionLocal()
@@ -50,3 +51,59 @@ async def scan_url_(user_id: int, url: str):
         return new_scan_history
     else:
         return "Url doesn't exit!!"
+
+# Kiểm tra số lần quét của người dùng theo ngày, tuần, tháng
+@router.get("/{user_id}/{month}/{year}/monthly-scanned")
+async def get_scan_history_count(user_id: int, month: int, year: int):
+    scan_history_count = db.query(func.count(scan_history.id)).\
+        filter(scan_history.user_id == user_id).\
+        filter(extract('month', scan_history.scan_time) == month).\
+        filter(extract('year', scan_history.scan_time) == year).\
+        scalar()
+    return {"scan_history_count": scan_history_count}
+
+@router.get("/{user_id}/{year}/week/{week_number}daily-scanned")
+async def get_scan_history_count(user_id: int, year: int, week_number: int):
+    scan_history_count = db.query(func.count(scan_history.id)). \
+        filter(scan_history.user_id == user_id). \
+        filter(extract('year', scan_history.scan_time) == year). \
+        filter(func.week(scan_history.scan_time) == week_number). \
+        scalar()
+    return {"scan_history_count": scan_history_count}
+
+@router.get("/{user_id}/{day}/{month}/{year}/daily-scanned")
+async def get_scan_history_count(user_id: int, day: int, month: int, year: int):
+    scan_history_count = db.query(func.count(scan_history.id)).\
+        filter(scan_history.user_id == user_id).\
+        filter(extract('day', scan_history.scan_time) == day).\
+        filter(extract('month', scan_history.scan_time) == month).\
+        filter(extract('year', scan_history.scan_time) == year).\
+        scalar()
+    return {"scan_history_count": scan_history_count}
+
+# Trả về danh sách lọc đã quét trong tháng
+@router.get("/{user_id}/monthly-scanned")
+async def getMonthlyScanned(user_id: int, month: int, year: int):
+    user = db.query(users).get(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    scan_ = db.query(scan_history.user_id, scan_history.url, scan_history.result, scan_history.scan_time,).\
+        filter(scan_history.user_id == user_id).\
+        filter(extract('month', scan_history.scan_time) == month).\
+        filter(extract('year', scan_history.scan_time) == year)
+    return {"scan_history": scan_.all()}
+
+# Trả về danh sách lọc đã quét trong ngày
+@router.get("/{user_id}/daily-scanned")
+async def getMonthlyScanned(user_id: int, day:int, month: int, year: int):
+    user = db.query(users).get(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    scan_ = db.query(scan_history.user_id, scan_history.url, scan_history.result, scan_history.scan_time,).\
+        filter(scan_history.user_id == user_id).\
+        filter(extract('day', scan_history.scan_time) == day).\
+        filter(extract('month', scan_history.scan_time) == month).\
+        filter(extract('year', scan_history.scan_time) == year)
+    return {"scan_history": scan_.all()}
--- apis/packages/database.py
+++ apis/packages/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, NVARCHAR, TIMESTAMP
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
 # Đường dẫn url link db
 db_url = 'mysql+pymysql://root:Ducanh23%40@localhost:3306/urldb'
 engine = create_engine(db_url)
@@ -16,6 +17,7 @@ class users(Base):
     password = Column(String(255))
     email = Column(String(255))
     is_admin = Column(Integer)
+    scan_history = relationship("scan_history", back_populates="user")
 
 class urls(Base):
     __tablename__ = 'urls'
@@ -30,6 +32,7 @@ class scan_history(Base):
     result = Column(Integer)
     url = Column(String(255))
     user_id = Column(Integer, ForeignKey(users.id))
+    user = relationship("users", back_populates="scan_history")
 
 class blacklist(Base):
     __tablename__ = 'blacklist'
